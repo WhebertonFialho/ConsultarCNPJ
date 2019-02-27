db05e9229dc0bc692d3779bb9f38fa602fe1432c
Wheberton Fialho <whebertonfialho@gmail.com>
2019-02-26 22:16:34 -0400

    Config Requeriments

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request, render_template
 from flask_bootstrap import Bootstrap
 from API import ConsultaCNPJ
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 @app.route('/index')
