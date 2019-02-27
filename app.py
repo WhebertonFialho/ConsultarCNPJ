b5dc8598a787d9abed002c9b945b71a26f5a8773
Your Name <you@example.com>
2019-02-27 17:17:35 -0400

    Config

--- app.py
+++ app.py
@@ -24,5 +24,5 @@ def cnpj_invalida():
 	
 if __name__ == '__main__':
 	Bootstrap(app)
-    	port = int(os.environ.get("PORT", 5000))
-    	app.run(host="0.0.0.0", port=port, debug=True)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host="0.0.0.0", port=port, debug=True)
